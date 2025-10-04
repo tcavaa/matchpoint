0a71a6c370fcd55ec59a0d79cc2a2114e38b4030
tornike-tsava-rgb <tornike.tsava@scandiweb.com>
2025-10-04 18:18:49 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export const TABLE_COUNT = 9;
 export const LOCAL_STORAGE_TABLES_KEY = "pingPongTablesData_v2";
 export const LOCAL_STORAGE_HISTORY_KEY = "pingPongSessionHistory_v1";
 export const LOCAL_STORAGE_SALES_SETTINGS_KEY = "pingPongSalesSettings_v1";
-export const APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwqrdjCYi_vOHDNlzzGFrSovu70ixtrDF8qBbmTMBcXvz6EWonQjmZmM7jT0VLkDnsrmQ/exec";
+export const APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwKQvjU7xHbtQnI3WazN9zetoG-4WVLOPmRQ-Yffpz7_SWn4vKmejw5lE00WPVzrzu2/exec";
 export const API_URL = 'https://ping.rretrocar.ge/api/items';
 export const LOCAL_STORAGE_MENU_ORDER_KEY = "pingPongMenuOrder_v1";
